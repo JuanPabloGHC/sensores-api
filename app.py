273cf0e2dcdde6eb384fef8ed48d7831d1b571c7
C4ncino <193305@iberopuebla.mx>
2023-04-26 15:04:43 -0600

    Stp Up for Render

--- app.py
+++ app.py
@@ -7,39 +7,52 @@ app = Flask(__name__)
 uriS = '/api/sensores'
 uriU = '/api/usuarios'
 
-with open('data.json', 'r') as file:
-    data = json.load(file)
+def openFile():
+    with open('data.json', 'r') as file:
+        data = json.load(file)
+    return data
 
-with open('users.json', 'r') as file:
-    users = json.load(file)
+def save(data):
+    with open('data.json', 'w') as file:
+        json.dump(data, file)
 
 #HOME----------------------------------------------------------------
-#GET
+#GET ALL
 @app.route(uriS, methods=['GET'])
 def home():
+    data = openFile()
+
     device_eui = '2CF7F1C04350014D'
     user = 'NAM2OM6AZSLWE733'
     password = '29F2DB070C804D8BA5D2DB268452BE24517C9789F458452CA199022C3C94EA1A'
     response = get(f'https://sensecap.seeed.cc/openapi/view_latest_telemetry_data?device_eui={device_eui}', auth = (user, password)).json()
     print(response)
+    
     return jsonify(data)
 
 #POST
-@app.route(uriS + '/<int:id>/<float:value>', methods=['GET'])
-def show_values(id, value):
+@app.route(uriS + '/<int:id>/<float:value>', methods=['POST'])
+def add_value(id, value):
+    data = openFile()
+
     #Buscar sensor en el json
     this_sensor = [sensor for sensor in data if sensor['id'] == id]
     if this_sensor:
         this_sensor[0]["values"].append(value)
+        
+        save(data)
     else:
         abort(404)
     
-    return jsonify(this_sensor[0])
+    return "ok"
 
 #USUARIOS-----------------------------------------------------------
 #GET
 @app.route(uriU, methods=['GET', 'POST'])
 def usuarios():
+    with open('users.json', 'r') as file:
+        users = json.load(file)
+
     if request.method == 'POST':
         if request.json:
             if ((request.json["username"] != '' and not(request.json["username"].isspace())) and (request.json["password"] != '' and not(request.json["password"].isspace()))):
